4676fa67063a7268591d1f8ef97fea35206c5a42
Ercan Er <iosyazilim@gmail.com>
2017-07-24 20:37:25 +0300

    ilk commit

---
+++ src/main/java/com/saman/agario/vector/Vector.java
@@ -0,0 +1,24 @@
+package com.saman.agario.vector;
+
+public class Vector {
+	
+public double x;
+public double y;
+
+
+public Vector(double x, double y) {
+	super();
+	this.x = x;
+	this.y = y;
+}
+
+public void normalize() {
+	
+}
+
+public double distanceTo(Vector other){
+	
+	return 5.2;
+}
+
+}
---
+++ src/test/java/com/saman/agario/vector/Testvector.java
@@ -0,0 +1,5 @@
+package com.saman.agario.vector;
+
+public class Testvector {
+
+}
