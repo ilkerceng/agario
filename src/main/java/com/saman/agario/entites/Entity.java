230bc333f13bcd75316a0d569e4dddf6fe05d5e1
Ercan Er <iosyazilim@gmail.com>
2017-07-24 21:26:03 +0300

    Entity Push

--- src/main/java/com/saman/agario/entites/Entity.java
+++ src/main/java/com/saman/agario/entites/Entity.java
@@ -1,5 +1,38 @@
 package com.saman.agario.entites;
 
+import java.awt.Color;
+
+import com.saman.agario.patterns.strategies.StepStrategy;
+import com.saman.agario.vector.Vector;
+
 public abstract class Entity {
+
+	public Vector direction;
+	public Vector location;
+	public double speed;
+	public double mass;
+	public Color color;
+	public StepStrategy stepStrategy;
+
+	public Vector getDirection() {
+		return direction;
+	}
+
+	public void setDirection(Vector direction) {
+		this.direction = direction;
+	}
+
+	public Vector getLocation() {
+		return location;
+	}
+
+	public void setLocation(Vector location) {
+		this.location = location;
+	}
+	
+	public void setXY(double x, double y){
+		this.location.setX(x);
+		this.location.setY(y);
+	}
 
 }
--- src/main/java/com/saman/agario/vector/Vector.java
+++ src/main/java/com/saman/agario/vector/Vector.java
@@ -1,29 +1,49 @@
 package com.saman.agario.vector;
 
 public class Vector {
-	
-public double x;
-public double y;
 
+	public double x;
+	public double y;
 
-public Vector(double x, double y) {
-	super();
-	this.x = x;
-	this.y = y;
-}
+	public Vector(double x, double y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
 
-public void normalize() {
-	
-}
+	public void normalize() {
 
-public double distanceTo(Vector other){
-	
-	double distX = this.x - other.x;
-	double distY = this.y - other.y;
-	
-	double distance = Math.sqrt(distX * distX + distY * distY);
-	
-	return distance;
-}
+		double length = Math.sqrt(this.x * this.x + this.y * this.y);
+		this.x /= length;
+		this.y /= length;
 
+	}
+
+	public double distanceTo(Vector other) {
+
+		double distX = this.x - other.x;
+		double distY = this.y - other.y;
+
+		double distance = Math.sqrt(distX * distX + distY * distY);
+
+		return distance;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	
 }
