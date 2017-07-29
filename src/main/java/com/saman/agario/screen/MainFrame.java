852d297c85b97dcc9c7d5bf28d4588c012d04ef9
ilker yıldırım <ilker.ceng@gmail.com>
2017-07-29 20:16:25 +0300

    Main Frame
    Main Frame is created for initial for screen, adding type of  Display attribute in it.

--- src/main/java/com/saman/agario/screen/MainFrame.java
+++ src/main/java/com/saman/agario/screen/MainFrame.java
@@ -1,18 +1,24 @@
 package com.saman.agario.screen;
 
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
-public class MainFrame extends JFrame {
+import com.saman.agario.game.Environment;
 
-	/**
-	 * 
-	 */
+public class MainFrame extends JFrame {
+	public Display display;
+	public Environment environment;
 	private static final long serialVersionUID = 1L;
 
-	public MainFrame() {
-		this.setTitle("Agar.IO Game");
+	public MainFrame(Environment environment) {
+		this.environment = environment;
+		this.setTitle(environment.gameName);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.display = new Display(environment);
+		this.getContentPane().add(display);
+		this.setSize(environment.windowWidth, environment.windowHeight);
 	}
 
 }
